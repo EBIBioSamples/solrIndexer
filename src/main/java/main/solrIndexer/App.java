ad7061dff5b000bee4c0d7a1d81da2929dc72122
fpenim <flaviapenim@gmail.com>
2015-09-29 13:42:56 +0100

    Added log4j2 configurations

--- src/main/java/main/solrIndexer/App.java
+++ src/main/java/main/solrIndexer/App.java
@@ -1,13 +1,18 @@
 package main.solrIndexer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+	
+	private static final Logger log = LogManager.getLogger();
+	
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        log.debug("log test");
     }
 }
