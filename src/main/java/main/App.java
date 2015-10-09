e84a5fc0ee23b8dcec7e35f47dc4899630b67cf2
fpenim <flaviapenim@gmail.com>
2015-10-09 15:57:05 +0100

    Minor adjustments

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,8 +1,12 @@
 package main;
 
 
+import javax.persistence.EntityManagerFactory;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import uk.ac.ebi.fg.core_model.resources.Resources;
 
 public class App {
 	private static final Logger logger = LogManager.getLogger(App.class.getName());
@@ -10,7 +14,7 @@ public class App {
     public static void main( String[] args ) {
     	logger.info("Entering application.");
 
-		//EntityManagerFactory factory = Resources.getInstance().getEntityManagerFactory();
+		EntityManagerFactory factory = Resources.getInstance().getEntityManagerFactory();
 
     }
 
