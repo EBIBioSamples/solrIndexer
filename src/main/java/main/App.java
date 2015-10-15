2107bb95e6288df8b7a3e417685eeca864e3790e
fpenim <flaviapenim@gmail.com>
2015-10-15 11:18:25 +0100

    Updates

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,21 +1,15 @@
 package main;
 
-
-import javax.persistence.EntityManagerFactory;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import uk.ac.ebi.fg.core_model.resources.Resources;
-
 public class App {
 	private static final Logger logger = LogManager.getLogger(App.class.getName());
-	
+
     public static void main( String[] args ) {
     	logger.info("Entering application.");
 
-		EntityManagerFactory factory = Resources.getInstance().getEntityManagerFactory();
-
+    	DataBaseConnection dbc = new DataBaseConnection();
     }
 
 }
--- src/main/java/main/DataBaseConnection.java
+++ src/main/java/main/DataBaseConnection.java
@@ -1,17 +1,54 @@
 package main;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class DataBaseConnection {
-	static final Logger logger = LogManager.getLogger(DataBaseConnection.class.getName());
+import uk.ac.ebi.fg.biosd.model.organizational.BioSampleGroup;
+import uk.ac.ebi.fg.core_model.resources.Resources;
 
-	public boolean doIt() {
-		logger.entry();
-		logger.trace("trace");
-		logger.error("did it again");
-		return logger.exit(false);
+public class DataBaseConnection {
+	static final Logger log = LogManager.getLogger(DataBaseConnection.class.getName());
+
+	private static EntityManagerFactory factory;
+	@PersistenceUnit(unitName="BioSampleGroup")
+	private static EntityManager manager = null;
+	private static EntityTransaction transaction = null;
+
+	public DataBaseConnection() {
+		try {
+    		factory = Resources.getInstance().getEntityManagerFactory();
+    		log.info("factory: " + factory);
+    		manager = factory.createEntityManager();
+    		log.info("manager: " + manager);
+    		transaction = manager.getTransaction();
+    		transaction.begin();
+
+    		CriteriaBuilder criteria = manager.getCriteriaBuilder();
+    		CriteriaQuery<BioSampleGroup> queryGroup = criteria.createQuery(BioSampleGroup.class);
+    		List<BioSampleGroup> groups = manager.createQuery(queryGroup.select(queryGroup.from(BioSampleGroup.class))).getResultList();
+    		for (int i = 0; i < 10; i++)
+    			log.info(i + " - " + groups.get(i));
+    		
+    	} catch (Exception e) {
+    		if(transaction != null && transaction.isActive()) {
+    			transaction.rollback();
+    			e.printStackTrace();
+    		}
+    		e.printStackTrace();
+    	} finally {
+    		if (manager != null && manager.isOpen()) {
+    			manager.close();
+    		}
+    	}
 	}
 
-	
 }
