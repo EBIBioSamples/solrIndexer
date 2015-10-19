b22b8ac422b8d8b94241ccc932d4a7c4f93d2e55
fpenim <flaviapenim@gmail.com>
2015-10-19 09:48:10 +0100

    Reorganization of the fetching process

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import uk.ac.ebi.fg.biosd.model.expgraph.BioSample;
+import uk.ac.ebi.fg.biosd.model.organizational.BioSampleGroup;
 
 public class App {
 	private static Logger log = LogManager.getLogger (App.class.getName());
@@ -9,7 +14,32 @@ public class App {
     public static void main( String[] args ) {
     	log.info("Entering application.");
 
-    	DataBaseConnection dbc = new DataBaseConnection();
+    	DataBaseInteraction dbi = null;
+    	try {
+    		dbi = new DataBaseInteraction();
+        	List<BioSampleGroup> groups = dbi.fetchGroups();
+        	//debugging purposes
+        	int i = 0;
+        	for (BioSampleGroup bsg : groups) {
+        		System.out.println(bsg.getReleaseDate());
+        		if (++i == 10) break;
+        	}
+
+        	List<BioSample> samples = dbi.fetchSamples();
+        	//debugging purposes
+        	int j = 0;
+        	for (BioSample bs : samples) {
+        		System.out.println(bs.getAcc());
+        		if (++j == 10) break;
+        	}
+
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+        	if (dbi.getEntityManager()!= null && dbi.getEntityManager().isOpen()) {
+        		dbi.getEntityManager().close();
+    		}
+    	}
     }
 
 }
