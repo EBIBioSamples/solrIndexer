0c728a70a23553c4c02b2bded90c3580262e5383
fpenim <flaviapenim@gmail.com>
2015-11-13 12:42:55 +0000

    tests corrections

--- src/test/java/uk/ac/ebi/solrIndexer/mainTest/BioSDEntitiesTest.java
+++ src/test/java/uk/ac/ebi/solrIndexer/mainTest/BioSDEntitiesTest.java
@@ -1,14 +1,12 @@
 package uk.ac.ebi.solrIndexer.mainTest;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class BioSDEntitiesTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
--- src/test/java/uk/ac/ebi/solrIndexer/mainTest/SolrIndexerTest.java
+++ src/test/java/uk/ac/ebi/solrIndexer/mainTest/SolrIndexerTest.java
@@ -1,14 +1,12 @@
 package uk.ac.ebi.solrIndexer.mainTest;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class SolrIndexerTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
