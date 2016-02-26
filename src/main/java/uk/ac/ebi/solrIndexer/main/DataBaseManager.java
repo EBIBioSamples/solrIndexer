f06b2782a528de647c1fbd92f34f1caef13476ed
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-02-26 14:53:28 +0000

    Added methods for counts of samples and groups

--- src/main/java/uk/ac/ebi/solrIndexer/main/DataBaseManager.java
+++ src/main/java/uk/ac/ebi/solrIndexer/main/DataBaseManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -16,6 +17,8 @@ import uk.ac.ebi.fg.biosd.annotator.persistence.AnnotatorAccessor;
 import uk.ac.ebi.fg.biosd.model.expgraph.BioSample;
 import uk.ac.ebi.fg.biosd.model.organizational.BioSampleGroup;
 import uk.ac.ebi.fg.core_model.expgraph.properties.ExperimentalPropertyValue;
+import uk.ac.ebi.fg.core_model.persistence.dao.hibernate.toplevel.AccessibleDAO;
+import uk.ac.ebi.fg.core_model.resources.Resources;
 import uk.ac.ebi.fg.core_model.terms.OntologyEntry;
 
 public class DataBaseManager {
@@ -61,6 +64,26 @@ public class DataBaseManager {
 
 		connection.closeDataBaseConnection();
 		return result;
+	}
+
+	public static int getGroupCount() {
+	    EntityManagerFactory emf = Resources.getInstance().getEntityManagerFactory();
+	    EntityManager em = emf.createEntityManager();
+	    AccessibleDAO<BioSampleGroup> dao = new AccessibleDAO<>(BioSampleGroup.class, em);
+	    //cast this to int
+	    //unlikely to really need a long for this! ~2 billion
+	    int count = (int) dao.count();
+	    return count;
+	}
+
+	public static int getSampleCount() {
+	    EntityManagerFactory emf = Resources.getInstance().getEntityManagerFactory();
+	    EntityManager em = emf.createEntityManager();
+	    AccessibleDAO<BioSample> dao = new AccessibleDAO<>(BioSample.class, em);
+	    //cast this to int
+	    //unlikely to really need a long for this! ~2 billion
+	    int count = (int) dao.count();
+	    return count;
 	}
 
 	@SuppressWarnings("rawtypes")
