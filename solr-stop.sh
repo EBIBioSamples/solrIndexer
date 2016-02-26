b1f548059294b9215db215a6438be64cf580b22b
Tony Burdett <tburdett@ebi.ac.uk>
2016-02-26 10:42:09 +0000

    solr stop port argument

--- solr-stop.sh
+++ solr-stop.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
+while getopts ":p:h" opt; do
+  case ${opt} in
+    p )
+      port=$OPTARG
+      ;;
+    h)
+      echo "Usage: solr-stop [-p]"
+      echo "    -p  Specify port of Solr instance to stop (defaults to 8983)"
+      exit 0;
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG"
+      exit 1;
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument"
+      ;;
+  esac
+done
+
 # check SOLR_DIR has been set
 if [ -z $SOLR_DIR ]
   then
     echo '$SOLR_DIR not set - please set this to the location of your Solr installation' >&2
     exit 1
   else
-    echo "Stopping Solr using SOLR_DIR = $SOLR_DIR"
-    $SOLR_DIR/bin/solr stop -p 8983
+    if [ ${port} ]
+      then
+        echo "Stopping Solr using SOLR_DIR $SOLR_DIR, port $port"
+        $SOLR_DIR/bin/solr stop -p $port
+      else
+        echo "Stopping Solr using SOLR_DIR $SOLR_DIR"
+        $SOLR_DIR/bin/solr stop
+    fi
 fi
