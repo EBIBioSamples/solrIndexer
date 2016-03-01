227eeeaaf04e0cec11a57f341201e9f29c8d1fda
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-03-01 09:32:10 +0000

    Commented out beans
    Fixes creating callables by class only

--- src/main/java/uk/ac/ebi/solrIndexer/main/Config.java
+++ src/main/java/uk/ac/ebi/solrIndexer/main/Config.java
@@ -25,7 +25,7 @@ public class Config {
 	public static PropertySourcesPlaceholderConfigurer propertyConfigIn() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
-
+/*
 	//needed to inject callables
 	@Bean
 	public static GroupRepoCallable getGroupRepoCallable() {
@@ -37,7 +37,7 @@ public class Config {
 	public static SampleRepoCallable getSampleRepoCallable() {
 		return new SampleRepoCallable();		
 	}
-	
+*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Config.class, args);
