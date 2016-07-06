3747ce3e7870ea13b1e91bb068c1b5659d910f24
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-07-06 15:49:14 +0100

    Fix for package renaming

--- src/main/java/uk/ac/ebi/biosamples/solrindexer/Config.java
+++ src/main/java/uk/ac/ebi/biosamples/solrindexer/Config.java
@@ -10,31 +10,15 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 
 @SpringBootApplication
-//check the packages here for components
-@ComponentScan(basePackages = { "uk.ac.ebi.solrIndexer" })
 //turn on advanced proxy object creation so the multi-threading and repositories work
 @EnableAspectJAutoProxy(proxyTargetClass = true) 
 public class Config {
-	
-	
-	//this is needed to read nonstrings from propertis files
+		
+	//this is needed to read nonstrings from properties files
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertyConfigIn() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
-/*
-	//needed to inject callables
-	@Bean
-	public static GroupRepoCallable getGroupRepoCallable() {
-		return new GroupRepoCallable();		
-	}
-
-	//needed to inject callables
-	@Bean
-	public static SampleRepoCallable getSampleRepoCallable() {
-		return new SampleRepoCallable();		
-	}
-*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Config.class, args);
