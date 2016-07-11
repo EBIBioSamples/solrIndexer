0aa3fb73aa6feb4d65feda3b47f5e67a83d97ec6
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-07-11 09:33:44 +0100

    Fix to handle uniquing multiple files

--- load.sh
+++ load.sh
@@ -7,27 +7,32 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/load_env.sh
 #clean any existing content
 rm -rf $NEO_DATA/graph.db.tmp
 
-#ensure all files have unique lines
+#combine multiple csv files and make sure they have unique lines
 #do this in parallel for all files at once
-for FILENAME in $IMPORTER/output/*.csv ;
-do
- echo "Sorting and uniqing $FILENAME"
- bash -c "sort -u -i -o $FILENAME.tmp $FILENAME; mv -f $FILENAME.tmp $FILENAME" &
-done
+sort -u -i -o $IMPORTER/output/sample.csv $IMPORTER/output/sample.*.csv &
+sort -u -i -o $IMPORTER/output/group.csv $IMPORTER/output/group.*.csv &
+sort -u -i -o $IMPORTER/output/links.csv $IMPORTER/output/links.*.csv &
+sort -u -i -o $IMPORTER/output/membership.csv $IMPORTER/output/membership.*.csv &
+sort -u -i -o $IMPORTER/output/derivation.csv $IMPORTER/output/derivation.*.csv &
+sort -u -i -o $IMPORTER/output/childof.csv $IMPORTER/output/childof.*.csv &
+sort -u -i -o $IMPORTER/output/recuratedfrom.csv $IMPORTER/output/recuratedfrom.*.csv &
+sort -u -i -o $IMPORTER/output/sameas.csv $IMPORTER/output/sameas.*.csv &
+sort -u -i -o $IMPORTER/output/haslink_group.csv $IMPORTER/output/haslink_group.*.csv  &
+sort -u -i -o $IMPORTER/output/haslink_sample.csv $IMPORTER/output/haslink_sample.*.csv & 
 wait
 
 #create new content
 time nice $NEO4J_BIN/neo4j-import --bad-tolerance 10000 --into $NEO_DATA/graph.db.tmp --i-type string \
-	--nodes:Sample "$IMPORTER/csv/sample_header.csv,`ls -1 $IMPORTER/output/sample.*csv | paste -sd ,`" \
-	--nodes:Group "$IMPORTER/csv/group_header.csv,`ls -1 $IMPORTER/output/group.*csv | paste -sd ,`" \
-	--nodes:ExternalLink "$IMPORTER/csv/links_header.csv,`ls -1 $IMPORTER/output/links.*csv | paste -sd ,`" \
-	--relationships:MEMBERSHIP "$IMPORTER/csv/membership_header.csv,`ls -1 $IMPORTER/output/membership.*csv | paste -sd ,`" \
-	--relationships:DERIVATION "$IMPORTER/csv/derivation_header.csv,`ls -1 $IMPORTER/output/derivation.*csv | paste -sd ,`" \
-	--relationships:CHILDOF "$IMPORTER/csv/childof_header.csv,`ls -1 $IMPORTER/output/childof.*csv | paste -sd ,`" \
-	--relationships:RECURATION "$IMPORTER/csv/recuratedfrom_header.csv,`ls -1 $IMPORTER/output/recuratedfrom.*csv | paste -sd ,`" \
-	--relationships:SAMEAS "$IMPORTER/csv/sameas_header.csv,`ls -1 $IMPORTER/output/sameas.*csv | paste -sd ,`" \
-	--relationships:HASLINK "$IMPORTER/csv/haslink_group_header.csv,`ls -1 $IMPORTER/output/haslink_group.*csv | paste -sd ,`" \
-	--relationships:HASLINK "$IMPORTER/csv/haslink_sample_header.csv,`ls -1 $IMPORTER/output/haslink_sample.*csv | paste -sd ,`" 
+	--nodes:Sample "$IMPORTER/csv/sample_header.csv,`ls -1 $IMPORTER/output/sample.csv | paste -sd ,`" \
+	--nodes:Group "$IMPORTER/csv/group_header.csv,`ls -1 $IMPORTER/output/group.csv | paste -sd ,`" \
+	--nodes:ExternalLink "$IMPORTER/csv/links_header.csv,`ls -1 $IMPORTER/output/links.csv | paste -sd ,`" \
+	--relationships:MEMBERSHIP "$IMPORTER/csv/membership_header.csv,`ls -1 $IMPORTER/output/membership.csv | paste -sd ,`" \
+	--relationships:DERIVATION "$IMPORTER/csv/derivation_header.csv,`ls -1 $IMPORTER/output/derivation.csv | paste -sd ,`" \
+	--relationships:CHILDOF "$IMPORTER/csv/childof_header.csv,`ls -1 $IMPORTER/output/childof.csv | paste -sd ,`" \
+	--relationships:RECURATION "$IMPORTER/csv/recuratedfrom_header.csv,`ls -1 $IMPORTER/output/recuratedfrom.csv | paste -sd ,`" \
+	--relationships:SAMEAS "$IMPORTER/csv/sameas_header.csv,`ls -1 $IMPORTER/output/sameas.csv | paste -sd ,`" \
+	--relationships:HASLINK "$IMPORTER/csv/haslink_group_header.csv,`ls -1 $IMPORTER/output/haslink_group.csv | paste -sd ,`" \
+	--relationships:HASLINK "$IMPORTER/csv/haslink_sample_header.csv,`ls -1 $IMPORTER/output/haslink_sample.csv | paste -sd ,`" 
 	
 	
 #Indexes are not created during the import. Instead you’ll need to add indexes afterwards
